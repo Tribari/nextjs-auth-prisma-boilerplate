6ffdb3eba3d3aa15c1afaf11ebd542165277f26d
Marco Kähler <m.kaehler@internetagentur-kaehler.de>
2021-11-10 10:21:20 +0100

    next-auth email provider & prisma adapter

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,26 @@
+import NextAuth from "next-auth";
+import EmailProvider from "next-auth/providers/email";
+
+import { NextApiHandler } from "next";
+import { PrismaClient } from "@prisma/client";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+const prisma = new PrismaClient();
+
+export default NextAuth({
+    providers: [
+        EmailProvider({
+            server: {
+                host: process.env.EMAIL_SERVER_HOST,
+                port: process.env.EMAIL_SERVER_PORT,
+                auth: {
+                  user: process.env.EMAIL_SERVER_USER,
+                  pass: process.env.EMAIL_SERVER_PASSWORD
+                }
+              },
+              from: process.env.EMAIL_FROM
+        }),
+    ],
+    adapter: PrismaAdapter(prisma),
+    secret: process.env.SECRET,
+});
